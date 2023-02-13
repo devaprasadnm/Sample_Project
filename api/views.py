997f67172bc19013933e5180b4b4087c0422e032
Devaprasad <devaprasadmohan@gmail.com>
2023-02-13 17:31:36 +0530

    mixins implemented

--- api/views.py
+++ api/views.py
@@ -5,47 +5,67 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics,mixins
 
-class ArticleList(APIView):
+class ArticleList(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin):
+    queryset  = Article.objects.all()
+    serializer_class = ArticleSerializer
 
     def get(self,request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles,many=True)
-        return Response(serializer.data)
+        return self.list(request)
 
     def post(self,request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status= status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
-class ArticleDetails(APIView):
+    # def get(self,request):
+    #     articles = Article.objects.all()
+    #     serializer = ArticleSerializer(articles,many=True)
+    #     return Response(serializer.data)
+    #
+    # def post(self,request):
+    #     serializer = ArticleSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status= status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get_object(self,id):
-        try:
-            return Article.objects.get(id=id)
-        except Article.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+class ArticleDetails(generics.GenericAPIView,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    queryset  = Article.objects.all()
+    serializer_class = ArticleSerializer
+    lookup_field = 'id'
 
-    def get(self,request,id):
-        article = self.get_object(id)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+    def get(self,reuest,id):
+        return self.retrieve(reuest,id=id)
 
     def put(self,request,id):
-        article = self.get_object(id)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return  self.update(request,id = id)
 
-    def delete(self,request,id):
-        article = self.get_object(id)
-        article.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request,id):
+        return self.destroy(request,id)
+
+    # def get_object(self,id):
+    #     try:
+    #         return Article.objects.get(id=id)
+    #     except Article.DoesNotExist:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)
+    #
+    # def get(self,request,id):
+    #     article = self.get_object(id)
+    #     serializer = ArticleSerializer(article)
+    #     return Response(serializer.data)
+    #
+    # def put(self,request,id):
+    #     article = self.get_object(id)
+    #     serializer = ArticleSerializer(article, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # def delete(self,request,id):
+    #     article = self.get_object(id)
+    #     article.delete()
+    #     return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 #
